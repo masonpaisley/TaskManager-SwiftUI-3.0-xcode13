b471d95b4cd66b2cc1120ec521993a8b4b5d622f
masonpaisley <masonpaisleyy@gmail.com>
2022-08-31 10:40:25 +0800

    Initial Commit

---
+++ Shared/TaskManagerApp.swift
@@ -0,0 +1,20 @@
+//
+//  TaskManagerApp.swift
+//  Shared
+//
+//  Created by jinzhao wang on 2022/8/31.
+//
+
+import SwiftUI
+
+@main
+struct TaskManagerApp: App {
+    let persistenceController = PersistenceController.shared
+
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+                .environment(\.managedObjectContext, persistenceController.container.viewContext)
+        }
+    }
+}
